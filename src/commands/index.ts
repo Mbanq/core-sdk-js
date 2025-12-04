1432d762c115c41a62823d12433d2410e5c4cd2f
Chea Chandarith <darith0001@gmail.com>
2025-12-04 15:19:52 +0700

    feat: introduce dedicated account statement commands and types, refactoring related logic from account commands.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,8 +3,9 @@ export { GetPermittedDocumentTypes, CreateClientIdentifier, UpdateClientIdentifi
 export { GetClient, UpdateClient, CreateClient, GetClients, DeleteClient, VerifyWithActivateClients, GetStatusOfVerifyClient } from './rest/client';
 export { MarkAsSuccess, MarkAsReturned, MarkAsFail, MarkAsProcessing, LogFailTransfer, GetTransfers, UpdateTraceNumber, CreateTransfer, GetTransfer } from './rest/transfer';
 export { GetPayment, GetPayments, CreatePayment, UpdatePayment, DeletePayment } from './rest/payment';
-export { GetAccount, GetAccountsOfClient, UpdateAccount, DeleteAccount, CreateAndActivateAccount, CloseAccount, BlockAccount, ScheduleAccountClosure, HoldAmount, GenerateAccountStatement, DownloadAccountStatement } from './rest/account';
+export { GetAccount, GetAccountsOfClient, UpdateAccount, DeleteAccount, CreateAndActivateAccount, CloseAccount, BlockAccount, ScheduleAccountClosure, HoldAmount } from './rest/account';
 export { CreateAccountProduct, UpdateAccountProduct, GetAllAccountProducts, GetAccountProductById } from './rest/accountProduct';
+export { GenerateAccountStatement, DownloadAccountStatement, GetAccountDocumentsDetails } from './rest/accountStatement';
 export { GraphQL } from './graphql';
 export { CustomGet, CustomCreate, CustomUpdate } from './rest/custom';
 export { GetRecipient, CreateRecipient, DeleteRecipient, GetRecipients } from './rest/recipient';
